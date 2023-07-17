8e9d08568c309bf08a8b0f0715bd25c6cd8486fc
Ninism <nin.alamo@outlook.com>
2023-07-17 10:15:40 +0800

    Addedservce

---
+++ sample/sample.infrastructure/RegisterService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using sample.domain.AggregateModels.CustomerAggregate;
+using sample.domain.Seedwork;
+using sample.infrastructure.AppSettings;
+using sample.infrastructure.CosmosDbData;
+using sample.infrastructure.CosmosDbData.Interfaces;
+
+namespace sample.infrastructure;
+
+public static class RegisterService
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, string endpoint,
+        string primaryKey, string databaseName, List<ContainerInfo> containers)
+    {
+        var client = new CosmosClient(endpoint, primaryKey);
+        var cosmosDbClientFactory = new CosmosDbContainerFactory(client, databaseName, containers);
+
+        services.AddSingleton<ICosmosDbContainerFactory>(cosmosDbClientFactory);
+
+
+        return services;
+    }
+}
+
